799761fc30ec1c491f5aa4a749648a5b82a8108e
Jiwon Choi <jwchoi.py@gmail.com>
2020-08-27 16:08:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'data')));
-app.use(express.static(path.join(__dirname, 'templates')));
-
 
 app.use('/', indexRouter);
 
