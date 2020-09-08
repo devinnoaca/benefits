5c498c7a97065c70e7dd5d581df911c43a5b1968
Terpe66 <45963139+Terpe66@users.noreply.github.com>
2020-09-08 13:48:27 +0900

    modify home.js viewport 오타 수정

--- templates/home.js
+++ templates/home.js
@@ -5,7 +5,7 @@ module.exports = function (navigation, masthead, program, partner, use, contact,
     
     <head>
       <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrinkf-to-fit=no" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
       <meta name="description" content="" />
       <meta name="author" content="" />
       <title>42 SEOUL BENEFITS</title>
