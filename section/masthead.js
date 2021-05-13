cc2f23576399af2e298805ffd027ab15e366e078
Ga-in Choi (LadyGaIn) <61079037+Ladygain@users.noreply.github.com>
2021-05-13 11:11:54 +0900

    운영 종료 메세지

--- section/masthead.js
+++ section/masthead.js
@@ -4,7 +4,7 @@ module.exports = function() {
       <div class="container">
         <div class="masthead-subheading">Welcome to our 42 SEOUL world!</div>
         <div class="masthead-heading text-uppercase">42 SEOUL BENEFITs</div>
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#partner">View Contents</a>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#partner">본 페이지는 5월 18일까지만 운영됩니다.</a>
       </div>
     </header>
   `
